f8df7d3843e762428356a5d2a20bd9eb651e5c3c
Mohamed S. Alkindi <msbakng@gmail.com>
2024-07-03 21:52:45 +0300

    Few changes and deleted old code on homepage view

--- lib/View/homepage_view.dart
+++ lib/View/homepage_view.dart
@@ -1,6 +1,8 @@
 // Main focus of this widget is to initialize the firebase once and for all views.
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:notetaking/View/email_verification_view.dart';
 import 'package:notetaking/View/login_view.dart';
 
 import '../firebase_options.dart';
@@ -20,19 +22,19 @@ class HomePage extends StatelessWidget {
         switch (snapshot.connectionState) {
           // If the ConnectionState returned 'Done'...
           case ConnectionState.done:
-            // final user = FirebaseAuth.instance.currentUser;
+            final user = FirebaseAuth.instance.currentUser;
 
-            // // Creating a boolean value that'll make sure that a user is not null and is signed in.
-            // final emailVerified = user?.emailVerified ?? false;
+            if (user != null) {
+              if (user.emailVerified) {
+                print('Email is verified');
+              } else {
+                return const EmailVerifyView();
+              }
+            } else {
+              return const LoginView();
+            }
+            return const Text('Done');
 
-            // if (emailVerified) {
-            //   // print('u r a verified user');
-            // } else {
-            //   // print('not verified');
-            //   return const EmailVerifyView();
-            // }
-            // return const Text('Done');
-            return const LoginView();
           // Otherwise show the text 'Loading'.
           default:
             return const CircularProgressIndicator();
