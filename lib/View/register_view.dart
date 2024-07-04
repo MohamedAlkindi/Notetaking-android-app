fe5e8eafa65b08e3f06d04fc047217824d96b6a8
Mohamed S. Alkindi <msbakng@gmail.com>
2024-07-04 15:03:20 +0300

    Removed a wrong widget

--- lib/View/register_view.dart
+++ lib/View/register_view.dart
@@ -42,115 +42,111 @@ class _RegisterViewState extends State<RegisterView> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Scaffold(
-        appBar: AppBar(title: const Text('Register')),
-        body: Center(
-          child: Column(
-            // makes all the column elements in the middle.
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                width: 450,
-                margin: const EdgeInsets.fromLTRB(0, 0, 0, 15),
-                // Creating 2 'textboxes' one for email and the second for password.
-                // Email textbox.
-                child: TextField(
-                  keyboardType: TextInputType
-                      .emailAddress, // Adds the '@' symbol on keyboard.
-                  enableSuggestions: false,
-                  autocorrect: false,
-                  controller: _email,
-                  // Adding a placeholder..
-                  decoration: const InputDecoration(
-                    hintText: 'Enter Email ',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(10.0),
-                      ),
+    return Scaffold(
+      appBar: AppBar(title: const Text('Register')),
+      body: Center(
+        child: Column(
+          // makes all the column elements in the middle.
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 450,
+              margin: const EdgeInsets.fromLTRB(0, 0, 0, 15),
+              // Creating 2 'textboxes' one for email and the second for password.
+              // Email textbox.
+              child: TextField(
+                keyboardType: TextInputType
+                    .emailAddress, // Adds the '@' symbol on keyboard.
+                enableSuggestions: false,
+                autocorrect: false,
+                controller: _email,
+                // Adding a placeholder..
+                decoration: const InputDecoration(
+                  hintText: 'Enter Email ',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10.0),
                     ),
                   ),
                 ),
               ),
-              Container(
-                width: 450,
-                margin: const EdgeInsets.fromLTRB(0, 0, 0, 15),
-                // Password textbox.
-                child: TextField(
-                  obscureText: true,
-                  enableSuggestions: false,
-                  autocorrect: false,
-                  controller: _password,
-                  decoration: const InputDecoration(
-                      hintText: 'Enter Password ',
-                      border: OutlineInputBorder(
-                          borderRadius:
-                              BorderRadius.all(Radius.circular(10.0)))),
-                ),
+            ),
+            Container(
+              width: 450,
+              margin: const EdgeInsets.fromLTRB(0, 0, 0, 15),
+              // Password textbox.
+              child: TextField(
+                obscureText: true,
+                enableSuggestions: false,
+                autocorrect: false,
+                controller: _password,
+                decoration: const InputDecoration(
+                    hintText: 'Enter Password ',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10.0)))),
               ),
-              SizedBox(
-                width: 450,
-                child: TextField(
-                  obscureText: true,
-                  enableSuggestions: false,
-                  controller: _repeatPassword,
-                  decoration: const InputDecoration(
-                      hintText: 'Repeat Password ',
-                      border: OutlineInputBorder(
-                          borderRadius:
-                              BorderRadius.all(Radius.circular(10.0)))),
-                ),
+            ),
+            SizedBox(
+              width: 450,
+              child: TextField(
+                obscureText: true,
+                enableSuggestions: false,
+                controller: _repeatPassword,
+                decoration: const InputDecoration(
+                    hintText: 'Repeat Password ',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10.0)))),
               ),
-              Container(
-                margin: const EdgeInsets.fromLTRB(0, 10, 0, 0),
-                child: TextButton(
-                  onPressed: () async {
-                    // Must initialize Firebase before using it, and must use 'async' in function declaration and 'await' when calling the initializeApp function!
-                    await Firebase.initializeApp(
-                      options: DefaultFirebaseOptions.currentPlatform,
-                    );
+            ),
+            Container(
+              margin: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+              child: TextButton(
+                onPressed: () async {
+                  // Must initialize Firebase before using it, and must use 'async' in function declaration and 'await' when calling the initializeApp function!
+                  await Firebase.initializeApp(
+                    options: DefaultFirebaseOptions.currentPlatform,
+                  );
 
-                    // As the user clicks on the button create 2 variables and get the text from the text boxes using the TextEditingController.
-                    final inputEmail = _email.text;
-                    final inputPassword = _password.text;
-                    final inputRepeatPassword = _repeatPassword.text;
+                  // As the user clicks on the button create 2 variables and get the text from the text boxes using the TextEditingController.
+                  final inputEmail = _email.text;
+                  final inputPassword = _password.text;
+                  final inputRepeatPassword = _repeatPassword.text;
 
-                    // Must put await as this is a Future function, again.
-                    try {
-                      if (inputPassword == inputRepeatPassword) {
-                        await FirebaseAuth.instance
-                            .createUserWithEmailAndPassword(
-                                email: inputEmail, password: inputPassword);
-                      } else {
-                        print('Input same pass');
-                      }
-                    } on FirebaseAuthException catch (e) {
-                      if (e.code == 'invalid-email') {
-                        // print('Invalid Email.');
-                      } else if (e.code == 'weak-password') {
-                        // print('Weak password');
-                      } else if (e.code == 'email-already-in-use') {
-                        // print('Email already in use.');
-                      }
+                  // Must put await as this is a Future function, again.
+                  try {
+                    if (inputPassword == inputRepeatPassword) {
+                      await FirebaseAuth.instance
+                          .createUserWithEmailAndPassword(
+                              email: inputEmail, password: inputPassword);
+                    } else {
+                      // print('Input same pass');
                     }
-                  },
+                  } on FirebaseAuthException catch (e) {
+                    if (e.code == 'invalid-email') {
+                      // print('Invalid Email.');
+                    } else if (e.code == 'weak-password') {
+                      // print('Weak password');
+                    } else if (e.code == 'email-already-in-use') {
+                      // print('Email already in use.');
+                    }
+                  }
+                },
 
-                  style: TextButton.styleFrom(
-                    backgroundColor: const Color.fromARGB(224, 199, 201, 228),
-                    padding: const EdgeInsets.fromLTRB(40, 15, 40, 15),
-                  ),
-                  // 'child' is anything that u'll put inside the parent element, in this case TextButton is the parent and Text is the child.
-                  child: const Text('Register'),
+                style: TextButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(224, 199, 201, 228),
+                  padding: const EdgeInsets.fromLTRB(40, 15, 40, 15),
                 ),
+                // 'child' is anything that u'll put inside the parent element, in this case TextButton is the parent and Text is the child.
+                child: const Text('Register'),
               ),
-              TextButton(
-                  onPressed: () {
-                    Navigator.of(context)
-                        .pushNamedAndRemoveUntil('/login/', (route) => false);
-                  },
-                  child: const Text('Already a user? Click here! 🖐🏼')),
-            ],
-          ),
+            ),
+            TextButton(
+                onPressed: () {
+                  Navigator.of(context)
+                      .pushNamedAndRemoveUntil('/login/', (route) => false);
+                },
+                child: const Text('Already a user? Click here! 🖐🏼')),
+          ],
         ),
       ),
     );
