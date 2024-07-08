bc69b4557885c820dce1dffd795492f55daa1412
Mohamed S. Alkindi <msbakng@gmail.com>
2024-07-08 04:01:46 +0300

    TESTING 'Horror...'

--- lib/services/auth/auth_user.dart
+++ lib/services/auth/auth_user.dart
@@ -5,12 +5,14 @@ import 'package:flutter/material.dart';
 // It means that this class and it's subclasses internals won't be changed upon initialization.
 @immutable
 
+// Creating a named paramater to make it more understandable.
 // Abstract the firebase User with a new class that takes the firebase User.
 class AuthUser {
   final bool isEmailVerified;
-  const AuthUser(this.isEmailVerified);
+  const AuthUser({required this.isEmailVerified});
 
 // factory constructor to return a current instance of the logged in user.
 // take the user from the firebase, and only get the emailVerified value, then put that value in your class.
-  factory AuthUser.fromFirebase(User user) => AuthUser(user.emailVerified);
+  factory AuthUser.fromFirebase(User user) =>
+      AuthUser(isEmailVerified: user.emailVerified);
 }
