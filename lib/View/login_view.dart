514e45f3040ae4761ecdbb1f898a69d24435cee1
Mohamed S. Alkindi <msbakng@gmail.com>
2024-07-12 22:23:55 +0300

    Added scrollable column and keyboard enhancements to login and register pages

--- lib/View/login_view.dart
+++ lib/View/login_view.dart
@@ -18,11 +18,13 @@ class _LoginViewState extends State<LoginView> {
   late final TextEditingController _email;
   late final TextEditingController _password;
 
+  late FocusNode _myFocusNote;
   // But must create an initializer and a disposer manually.
   @override
   void initState() {
     _email = TextEditingController();
     _password = TextEditingController();
+    _myFocusNote = FocusNode();
     super.initState();
   }
 
@@ -30,6 +32,7 @@ class _LoginViewState extends State<LoginView> {
   void dispose() {
     _email.dispose();
     _password.dispose();
+    _myFocusNote.dispose();
     super.dispose();
   }
 
@@ -43,116 +46,125 @@ class _LoginViewState extends State<LoginView> {
         ),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'assets/images/login.png',
-              height: 180,
-            ),
+        // To make the column scrollable.
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/login.png',
+                height: 180,
+              ),
 
-            // Creating 2 'textboxes' one for email and the second for password, each in their own 'Container' to put some styling to them.
-            // Email textfield.
-            Container(
-              width: 450,
-              margin: const EdgeInsets.fromLTRB(0, 25, 0, 15),
-              child: TextField(
-                keyboardType: TextInputType.emailAddress,
-                enableSuggestions: false,
-                autocorrect: false,
-                controller: _email,
-                decoration: const InputDecoration(
-                  hintText: 'Enter Email ',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(10.0),
+              // Creating 2 'textboxes' one for email and the second for password, each in their own 'Container' to put some styling to them.
+              // Email textfield.
+              Container(
+                width: 450,
+                margin: const EdgeInsets.fromLTRB(0, 25, 0, 15),
+                child: TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  enableSuggestions: false,
+                  autocorrect: false,
+                  controller: _email,
+                  decoration: const InputDecoration(
+                    hintText: 'Enter Email ',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10.0),
+                      ),
                     ),
                   ),
+                  textInputAction: TextInputAction.next,
+                  onSubmitted: (term) {
+                    FocusScope.of(context).requestFocus(_myFocusNote);
+                  },
                 ),
               ),
-            ),
 
-            // 'SizedBox' is more suitable if u won't use properties of 'Container'. Otherwise use 'Container' as it has more properties to work with.
-            // Password textfield.
-            SizedBox(
-              width: 450,
-              child: TextField(
-                obscureText: true,
-                enableSuggestions: false,
-                autocorrect: false,
-                controller: _password,
-                decoration: const InputDecoration(
-                  hintText: 'Enter Password ',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(10.0),
+              // 'SizedBox' is more suitable if u won't use properties of 'Container'. Otherwise use 'Container' as it has more properties to work with.
+              // Password textfield.
+              SizedBox(
+                width: 450,
+                child: TextField(
+                  obscureText: true,
+                  enableSuggestions: false,
+                  autocorrect: false,
+                  controller: _password,
+                  decoration: const InputDecoration(
+                    hintText: 'Enter Password ',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10.0),
+                      ),
                     ),
                   ),
+                  focusNode: _myFocusNote,
+                  textInputAction: TextInputAction.done,
                 ),
               ),
-            ),
 
-            // Login button.
-            Container(
-              margin: const EdgeInsets.fromLTRB(0, 10, 0, 10),
-              child: TextButton(
-                onPressed: () async {
-                  // As the user clicks on the button create 2 variables and get the text from the text boxes using the TextEditingController.
-                  final inputEmail = _email.text;
-                  final inputPassword = _password.text;
+              // Login button.
+              Container(
+                margin: const EdgeInsets.fromLTRB(0, 10, 0, 10),
+                child: TextButton(
+                  onPressed: () async {
+                    // As the user clicks on the button create 2 variables and get the text from the text boxes using the TextEditingController.
+                    final inputEmail = _email.text;
+                    final inputPassword = _password.text;
 
-                  try {
-                    await AuthService.fireBase().logIn(
-                      email: inputEmail,
-                      password: inputPassword,
-                    );
+                    try {
+                      await AuthService.fireBase().logIn(
+                        email: inputEmail,
+                        password: inputPassword,
+                      );
 
-                    final loggedinUser = AuthService.fireBase().currentUser;
+                      final loggedinUser = AuthService.fireBase().currentUser;
 
-                    if (loggedinUser != null) {
-                      if (loggedinUser.isEmailVerified) {
-                        Navigator.of(context).pushNamedAndRemoveUntil(
-                          notesRoute,
-                          (_) => false,
-                        );
-                      } else {
-                        // Use pushNamed so u give the user an option to go back if he entered wrong details.
-                        Navigator.of(context).pushNamed(
-                          emailVerifyRoute,
-                        );
+                      if (loggedinUser != null) {
+                        if (loggedinUser.isEmailVerified) {
+                          Navigator.of(context).pushNamedAndRemoveUntil(
+                            notesRoute,
+                            (_) => false,
+                          );
+                        } else {
+                          // Use pushNamed so u give the user an option to go back if he entered wrong details.
+                          Navigator.of(context).pushNamed(
+                            emailVerifyRoute,
+                          );
+                        }
                       }
+                    } on NetworkExceptions {
+                      showErrorDialog(
+                          context, "Please check your internet connection.");
+                    } on AuthExceptions {
+                      showErrorDialog(
+                          context, "Please check your input details.");
+                    } on GenericAuthException {
+                      showErrorDialog(context, "An error has happened!");
                     }
-                  } on NetworkExceptions {
-                    showErrorDialog(
-                        context, "Please check your internet connection.");
-                  } on AuthExceptions {
-                    showErrorDialog(
-                        context, "Please check your input details.");
-                  } on GenericAuthException {
-                    showErrorDialog(context, "An error has happened!");
-                  }
-                },
-                style: TextButton.styleFrom(
-                  backgroundColor: const Color.fromARGB(224, 199, 201, 228),
-                  padding: const EdgeInsets.fromLTRB(40, 15, 40, 15),
-                ),
-                child: const Text(
-                  'Login',
+                  },
+                  style: TextButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(224, 199, 201, 228),
+                    padding: const EdgeInsets.fromLTRB(40, 15, 40, 15),
+                  ),
+                  child: const Text(
+                    'Login',
+                  ),
                 ),
               ),
-            ),
 
-            // Register view button.
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pushNamedAndRemoveUntil(
-                  registerRoute,
-                  (route) => false,
-                );
-              },
-              child: const Text('Not registered? Click here! 🖐🏼'),
-            ),
-          ],
+              // Register view button.
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pushNamedAndRemoveUntil(
+                    registerRoute,
+                    (route) => false,
+                  );
+                },
+                child: const Text('Not registered? Click here! 🖐🏼'),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/View/register_view.dart
+++ lib/View/register_view.dart
@@ -16,11 +16,15 @@ class _RegisterViewState extends State<RegisterView> {
   late final TextEditingController _password;
   late final TextEditingController _repeatPassword;
 
+  late FocusNode _focusNode1;
+  late FocusNode _focusNode2;
   @override
   void initState() {
     _email = TextEditingController();
     _password = TextEditingController();
     _repeatPassword = TextEditingController();
+    _focusNode1 = FocusNode();
+    _focusNode2 = FocusNode();
     super.initState();
   }
 
@@ -29,6 +33,8 @@ class _RegisterViewState extends State<RegisterView> {
     _email.dispose();
     _password.dispose();
     _repeatPassword.dispose();
+    _focusNode1.dispose();
+    _focusNode2.dispose();
     super.dispose();
   }
 
@@ -42,130 +48,143 @@ class _RegisterViewState extends State<RegisterView> {
         ),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'assets/images/register.png',
-              height: 140,
-            ),
-            Container(
-              width: 450,
-              margin: const EdgeInsets.fromLTRB(0, 25, 0, 15),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/register.png',
+                height: 140,
+              ),
+              Container(
+                width: 450,
+                margin: const EdgeInsets.fromLTRB(0, 25, 0, 15),
 
-              // Email textfield.
-              child: TextField(
-                keyboardType: TextInputType
-                    .emailAddress, // Adds the '@' symbol on keyboard.
-                enableSuggestions: false,
-                autocorrect: false,
-                controller: _email,
-                // Adding a placeholder..
-                decoration: const InputDecoration(
-                  hintText: 'Enter Email ',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(10.0),
+                // Email textfield.
+                child: TextField(
+                  keyboardType: TextInputType
+                      .emailAddress, // Adds the '@' symbol on keyboard.
+                  enableSuggestions: false,
+                  autocorrect: false,
+                  controller: _email,
+                  // Adding a placeholder..
+                  decoration: const InputDecoration(
+                    hintText: 'Enter Email ',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10.0),
+                      ),
                     ),
                   ),
+                  textInputAction: TextInputAction.next,
+                  onSubmitted: (term) {
+                    FocusScope.of(context).requestFocus(_focusNode1);
+                  },
                 ),
               ),
-            ),
 
-            // Password textField.
-            Container(
-              width: 450,
-              margin: const EdgeInsets.fromLTRB(0, 0, 0, 15),
-              // Password textbox.
-              child: TextField(
-                obscureText: true,
-                enableSuggestions: false,
-                autocorrect: false,
-                controller: _password,
-                decoration: const InputDecoration(
-                  hintText: 'Enter Password ',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(10.0),
+              // Password textField.
+              Container(
+                width: 450,
+                margin: const EdgeInsets.fromLTRB(0, 0, 0, 15),
+                // Password textbox.
+                child: TextField(
+                  obscureText: true,
+                  enableSuggestions: false,
+                  autocorrect: false,
+                  controller: _password,
+                  decoration: const InputDecoration(
+                    hintText: 'Enter Password ',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10.0),
+                      ),
                     ),
                   ),
+                  focusNode: _focusNode1,
+                  textInputAction: TextInputAction.next,
+                  onSubmitted: (term) {
+                    FocusScope.of(context).requestFocus(_focusNode2);
+                  },
                 ),
               ),
-            ),
 
-            // Repeat pass textField.
-            SizedBox(
-              width: 450,
-              child: TextField(
-                obscureText: true,
-                enableSuggestions: false,
-                controller: _repeatPassword,
-                decoration: const InputDecoration(
-                  hintText: 'Repeat Password ',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(10.0),
+              // Repeat pass textField.
+              SizedBox(
+                width: 450,
+                child: TextField(
+                  obscureText: true,
+                  enableSuggestions: false,
+                  controller: _repeatPassword,
+                  decoration: const InputDecoration(
+                    hintText: 'Repeat Password ',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10.0),
+                      ),
                     ),
                   ),
+                  focusNode: _focusNode2,
+                  textInputAction: TextInputAction.done,
                 ),
               ),
-            ),
 
-            // Register button.
-            Container(
-              margin: const EdgeInsets.fromLTRB(0, 10, 0, 0),
-              child: TextButton(
-                onPressed: () async {
-                  final inputEmail = _email.text;
-                  final inputPassword = _password.text;
-                  final inputRepeatPassword = _repeatPassword.text;
+              // Register button.
+              Container(
+                margin: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                child: TextButton(
+                  onPressed: () async {
+                    final inputEmail = _email.text;
+                    final inputPassword = _password.text;
+                    final inputRepeatPassword = _repeatPassword.text;
 
-                  try {
-                    if (inputPassword == inputRepeatPassword) {
-                      await AuthService.fireBase().createUser(
-                          inputEmail: inputEmail,
-                          inputPassword: inputPassword,
-                          repeatPassword: inputRepeatPassword);
+                    try {
+                      if (inputPassword == inputRepeatPassword) {
+                        await AuthService.fireBase().createUser(
+                            inputEmail: inputEmail,
+                            inputPassword: inputPassword,
+                            repeatPassword: inputRepeatPassword);
 
-                      AuthService.fireBase().sendEmailVerification();
+                        AuthService.fireBase().sendEmailVerification();
 
-                      Navigator.of(context).pushNamed(
-                        emailVerifyRoute,
-                      );
-                    } else {
-                      showErrorDialog(context, "Passwords don't match.");
+                        Navigator.of(context).pushNamed(
+                          emailVerifyRoute,
+                        );
+                      } else {
+                        showErrorDialog(context, "Passwords don't match.");
+                      }
+                    } on NetworkExceptions {
+                      showErrorDialog(
+                          context, "Please check your internet connection.");
+                    } on AuthExceptions {
+                      showErrorDialog(
+                          context, "Please check your input details.");
+                    } on GenericAuthException {
+                      showErrorDialog(context, "An error has happened!");
                     }
-                  } on NetworkExceptions {
-                    showErrorDialog(
-                        context, "Please check your internet connection.");
-                  } on AuthExceptions {
-                    showErrorDialog(
-                        context, "Please check your input details.");
-                  } on GenericAuthException {
-                    showErrorDialog(context, "An error has happened!");
-                  }
-                },
-                style: TextButton.styleFrom(
-                  backgroundColor: const Color.fromARGB(224, 199, 201, 228),
-                  padding: const EdgeInsets.fromLTRB(40, 15, 40, 15),
-                ),
-                child: const Text(
-                  'Register',
+                  },
+                  style: TextButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(224, 199, 201, 228),
+                    padding: const EdgeInsets.fromLTRB(40, 15, 40, 15),
+                  ),
+                  child: const Text(
+                    'Register',
+                  ),
                 ),
               ),
-            ),
 
-            // Login view button.
-            TextButton(
-              onPressed: () {
-                Navigator.of(context)
-                    .pushNamedAndRemoveUntil(loginRoute, (route) => false);
-              },
-              child: const Text(
-                'Already a user? Click here! 🖐🏼',
+              // Login view button.
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context)
+                      .pushNamedAndRemoveUntil(loginRoute, (route) => false);
+                },
+                child: const Text(
+                  'Already a user? Click here! 🖐🏼',
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
